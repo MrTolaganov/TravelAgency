653180792e7d9362af1dad835d2c9f4ac08a24ec
Otabek <otabektolaganov69@gmail.com>
2023-11-26 17:52:57 +0500

    26/11/2023 17:22

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = document.querySelectorAll(".fadeIn");
+
+  function fadeInElements() {
+    elements.forEach((element) => {
+      if (isElementInViewport(element)) {
+        element.classList.add("visible");
+      }
+    });
+  }
+
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  window.addEventListener("scroll", fadeInElements);
+  window.addEventListener("resize", fadeInElements);
+
+  fadeInElements();
+});
